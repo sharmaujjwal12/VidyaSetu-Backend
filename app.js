2d57db6e70c715b8eba3bc4b8b8efbe36657d4c1
Ujjwal Sharma <us3566650@gmail.com>
2026-04-01 15:11:22 +0530

    Fix CORS for Vercel frontend and enable credentials

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-require('dotenv').config(); // ye .env file read karega
+require("dotenv").config(); // ye .env file read karega
 let dns = require("dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 const cors = require("cors");
 const session = require("express-session");
 const mongoDBStore = require("connect-mongodb-session")(session);
 let mongoose = require("mongoose");
-let MONGO_URL =
-  "mongodb+srv://ujjwal:ujjwal@ujjwal.wajx9iv.mongodb.net/VidyaSetu?appName=ujjwal";
+let MONGO_URL = process.env.MONGODB_URL;
 let bodyParser = require("body-parser");
 let express = require("express");
 let app = express();
@@ -19,14 +18,27 @@ const newsRouter = require("./routes/NewsRouter");
 const paymentRouter = require("./routes/Payment");
 
 // app.use(express.static(path.join(rootDir,'public')));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://vidya-setu-front-end.vercel.app",
+];
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   }),
 );
+
+// optional preflight handler
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -49,18 +61,17 @@ app.use(
 );
 
 app.use("/api/Quiz", UsersRouter);
-app.use("/api",paymentRouter)
+app.use("/api", paymentRouter);
 app.use("/api", hostRouter);
-app.use("/api/news",newsRouter);
-app.use("/api",videoRouter);
+app.use("/api/news", newsRouter);
+app.use("/api", videoRouter);
 app.get("/", (req, res) => {
   res.send("VidyaSetu Backend Running 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
-mongoose.connect(process.env.MONGODB_URI).then(() => {
+mongoose.connect(process.env.MONGODB_URL).then(() => {
   app.listen(PORT, () => {
     console.log(`Server Listening At : http://localhost:${PORT}`);
   });
 });
-
